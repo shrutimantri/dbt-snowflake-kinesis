d733c2fe2c556454246b21576a4046f0e4a6e1c8
Shruti Mantri <shruti.mantri18101@gmail.com>
2025-04-12 03:43:56 +0000

    Removing changes

--- macros/get_custom_schema.sql
+++
@@ -1,8 +0,0 @@
-{% macro generate_schema_name(custom_schema_name, node) -%}
-    {%- set default_schema = target.schema -%}
-    {%- if env_var('DBT_ENVIRONMENT') == 'dev' -%} 
-        {{ custom_schema_name | trim }}_{{ default_schema }}
-    {%- else -%}
-        {{ custom_schema_name | trim }}
-    {%- endif -%}
-{%- endmacro %}
